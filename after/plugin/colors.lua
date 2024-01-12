1687f34d6d7b45d92efb6ff536072c44e580846b
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-01-12 11:07:49 +0600

    Changed default color scheme from rose-pine to rose-pine-moon

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyAboba()
-    color = color or 'rose-pine'
+    color = color or 'rose-pine-moon'
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
