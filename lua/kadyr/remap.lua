b432b6ee1e937c446f438b8603996ca30acd9ca1
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-01-13 23:19:37 +0600

    Added remap to keep cursor in center when G

--- lua/kadyr/remap.lua
+++ lua/kadyr/remap.lua
@@ -6,6 +6,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "G", "Gzz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
