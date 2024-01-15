7a2cfbac4512ccf1867d2154195f5e3c4402f929
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-01-15 23:27:39 +0600

    Added remap for telescope to live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>ps', function()
