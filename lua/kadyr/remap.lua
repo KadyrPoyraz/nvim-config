6812030a315abd414e2c2460396606db91764458
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-01-24 13:56:26 +0600

    Added ACTUALLY the best remap of all time

--- lua/kadyr/remap.lua
+++ lua/kadyr/remap.lua
@@ -41,3 +41,5 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
