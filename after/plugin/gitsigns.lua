c763c1639089b8603185a384d1c20f9dffb8591d
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-02-09 23:42:30 +0600

    Added new remaps for gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,5 +1,7 @@
 require('gitsigns').setup()
 
 
-vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
+vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>")
 vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>")
+vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>")
+vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<CR>")
