6457d394fb382ea3b5a8e2570da10e744de0a86d
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-05-01 18:47:19 +0600

    Aboba

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,7 +13,7 @@ lsp_zero.on_attach(function(_, bufnr)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 require('mason').setup({})
